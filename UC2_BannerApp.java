ad71b0fe013c86ed0787c2792dd47d6c99cb0d6c
Shreyas <your_email@example.com>
2026-02-15 21:26:13 +0530

    Real code update

---
+++ UC2_BannerApp.java
@@ -0,0 +1,11 @@
+public class UC2_BannerApp {
+    public static void main(String[] args) {
+        System.out.println("  ***    ***    *****   ***** ");
+        System.out.println(" *   *  *   *   *    *  *     ");
+        System.out.println("*     **     *  *    *  *     ");
+        System.out.println("*     **     *  *****   ***** ");
+        System.out.println("*     **     *  *           * ");
+        System.out.println(" *   *  *   *   *           * ");
+        System.out.println("  ***    ***    *       ***** ");
+    }
+}
